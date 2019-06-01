506ce62db02a2c0cfe2fa51090be66f2ba232167
Your Name <you@example.com>
2019-06-01 11:29:41 -0400

    build

---
+++ precache-manifest.449fd4ebef920a27fa8d081610d6e82a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1eb974810c5e5034ba5",
+    "url": "/static/css/main.a9a17d9a.chunk.css"
+  },
+  {
+    "revision": "b1eb974810c5e5034ba5",
+    "url": "/static/js/main.b1eb9748.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/static/js/2.58f496bc.chunk.js"
+  },
+  {
+    "revision": "d535583efe781b27bf4ca279033058e7",
+    "url": "/index.html"
+  }
+];
